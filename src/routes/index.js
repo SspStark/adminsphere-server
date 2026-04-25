5a2493c7bf76ee8ef0b0909ed7415d2404cde772
SspStark <ssptonystark005@gmail.com>
2026-04-25 17:19:14 +0530

    refactor admin routes; move admin route handlers to userRoutes and remove adminRoutes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,8 @@
-import adminRoutes from "./adminRoutes.js";
 import authRoutes from "./authRoutes.js";
 import userRoutes from "./userRoutes.js"
 import taskRoutes from "./taskRoutes.js"
 
 export default function registerRoutes(app) {
-    app.use("/api/v1/admin", adminRoutes);
     app.use("/api/v1/auth", authRoutes);
     app.use("/api/v1/users", userRoutes);
     app.use("/api/v1/tasks", taskRoutes);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,32 @@
 import express from 'express';
-import { updateMe, changeMyPassword } from '../controllers/userController.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { isAdmin } from "../middlewares/isAdmin.js";
+import { createUserValidation, validateResult } from "../middlewares/validation.js";
+import uploadAvatar from '../middlewares/uploadAvatar.js'
+
+import { updateMe, changeMyPassword } from '../controllers/userController.js';
+
+import { createUser, updateUserByAdmin, resetUserPasswordByAdmin, deleteUserByAdmin, getAllUsers, getUserById, uploadUserAvatarByAdmin, deleteUserAvatarByAdmin } from "../controllers/adminController.js";
+import { downloadUsersPDF } from "../controllers/pdfController.js";
+import { downloadUsersExcel } from "../controllers/excelController.js";
 
 const router = express.Router();
 
+// Self routes
 router.patch("/me", authMiddleware, updateMe);
 router.patch("/me/password", authMiddleware, changeMyPassword);
+
+// Admin routes
+router.get("/reports/users-pdf", authMiddleware, isAdmin, downloadUsersPDF);
+router.get("/reports/users-excel", authMiddleware, isAdmin, downloadUsersExcel);
+
+router.post("/", authMiddleware, isAdmin, createUserValidation, validateResult, createUser);
+router.get("/", authMiddleware, isAdmin, getAllUsers);
+router.get("/:id", authMiddleware, isAdmin, getUserById);
+router.patch("/:id", authMiddleware, isAdmin, updateUserByAdmin);
+router.delete("/:id", authMiddleware, isAdmin, deleteUserByAdmin);
+router.patch("/:id/password", authMiddleware, isAdmin, resetUserPasswordByAdmin);
+router.patch("/:id/avatar", authMiddleware, isAdmin, uploadAvatar.single("avatar"), uploadUserAvatarByAdmin);
+router.delete("/:id/avatar", authMiddleware, isAdmin, deleteUserAvatarByAdmin);
 
 export default router;
